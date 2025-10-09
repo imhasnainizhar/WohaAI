0251d1e9325e1f830dacdc47949903eefa49acc6
Hasnain Izhar <26821517-TheHasnainIzhar.noreply.gitlab.com>
2025-10-09 12:17:23 -0700

    deleting old files

--- .main/nextapp/.next/static/chunks/src_app_me_page_tsx_a5a0fc7f._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_me_page_tsx_d3bf174c._.js"
-],
-    source: "dynamic"
-});
