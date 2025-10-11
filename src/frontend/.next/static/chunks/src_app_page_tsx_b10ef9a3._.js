ffde7bfa4cc6fde8c64b1fab4f3146a419b39bdf
Hasnain Izhar <26821517-TheHasnainIzhar.noreply.gitlab.com>
2025-10-11 14:38:11 -0700

    Commiting all docker work with resolving css types

--- src/frontend/.next/static/chunks/src_app_page_tsx_b10ef9a3._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_styles_pages_home_page_e5295830.css"
-],
-    source: "dynamic"
-});
