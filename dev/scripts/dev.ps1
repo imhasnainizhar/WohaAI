d45ea7a57441a274b4c15ebcffa5e29a34edc895
Hasnain Izhar <26821517-TheHasnainIzhar.noreply.gitlab.com>
2025-12-08 00:46:41 +0500

    finalizing dev env for local and docker setups, ready to work on features and bugs

--- dev/scripts/dev.ps1
+++ dev/scripts/dev.ps1
@@ -5,10 +5,10 @@ docker network create microservices_net_01
 docker compose `
   -f ../compose/client/compose.frontend.yml `
   -f ../compose/shared/compose.db.yml `
-  -f ../compose/shared/compose.utility.yml `
   -f ../compose/services/compose.ms.yml `
-  -f ../compose/infra/ngnix/compose.ngnix.yml `
+  -f ../compose/infra/nginx/compose.nginx.yml `
   -f ../compose/shared/compose.cache.yml `
   -f ../compose/shared/compose.queue.yml `
+  --env-file ../../.env `
   --env-file ../../.env.docker `
   up --build
--- dev/scripts/dev.sh
+++ dev/scripts/dev.sh
@@ -1,10 +1,13 @@
 ## For Linuux and MacOS
 
 docker compose \
-  -f ../compose/compose.shared.yml \
-  -f ../compose/compose.utility.yml\
-  -f ../compose/compose.db.yml \
-  -f ../compose/compose.auth.yml \
-  -f ../compose/compose.gateway.yml \
+  -f ../compose/client/compose.frontend.yml \
+  -f ../compose/shared/compose.db.yml \
+  -f ../compose/shared/compose.utility.yml \
+  -f ../compose/services/compose.ms.yml \
+  -f ../compose/infra/ngnix/compose.ngnix.yml \
+  -f ../compose/shared/compose.cache.yml \
+  -f ../compose/shared/compose.queue.yml \
   --env-file ../../.env \
+  --env-file ../../.env.docker \
   up --build
